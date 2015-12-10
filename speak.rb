d45734791e50a14434c3f00a20f533b5463d8839
samskinnerphd <samskinnerphd@gmail.com>
2015-12-10 09:51:45 -0500

    speak TJ

---
+++ speak.rb
@@ -0,0 +1,26 @@
+# loop do
+#   to_say = 'dance again t j dance again mutha fucka'
+#   `say  #{to_say}`
+# end
+
+
+1.times do
+	3.times do
+	  to_say = 'dance t j dance'
+	  `say  #{to_say}`
+	end
+	to_sing = 'now, sing'
+	`say #{to_sing}`
+		sleep(1)
+	2.times do
+	  to_say = 'sing t j sing'
+	  `say  #{to_say}`
+	end
+	to_swear = 'okay mother fucker'
+	`say #{to_swear}`
+	3.times do
+	  to_say = 'dance t j dance'
+	  `say  #{to_say}`
+	end
+end
+
